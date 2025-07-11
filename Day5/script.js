6b6602b000a55113bd4f5ef2352b914ac98e3128
RajJadhav4140 <rajjadhav4140@gmail.com>
2025-07-11 20:26:27 +0530

    learn conditional Statements

---
+++ Day5/script.js
@@ -0,0 +1,36 @@
+// conditional Statemets
+
+// 1) if statements
+
+let Number = 28
+
+if(Number % 2 == 0){
+    console.log(Number, "is even")
+}
+
+if(Number % 2 != 0){
+    console.log(Number, "is odd")
+}
+
+if(true){
+    console.log("this is the true")
+}
+
+// 2) if else Statemets
+
+let num = 49
+
+if(num % 2 == 0){
+    console.log(num, "is Even");
+}else{
+    console.log(num, "is odd");
+}
+
+
+let age = 18
+
+if(age >= 18){
+    console.log("You are eligible to vote");
+}else{
+    console.log("You are not eligible to vote")
+}
