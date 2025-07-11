1c36a44439263952c0d09138f93715806efbfa0f
RajJadhav4140 <rajjadhav4140@gmail.com>
2025-07-11 20:21:11 +0530

    learn Operators

---
+++ Day3/script.js
@@ -0,0 +1,140 @@
+// A) arithmetic operator
+ 
+   // i) + addition Operator
+
+   let First_Number = 20
+   let Second_Number = 30
+   let Result = First_Number + Second_Number
+   console.log(Result)
+
+   console.log(100 + 200)
+
+   // ii) - Substraction Operator
+
+   let X = 20
+   let Y = 30
+   let Z = (X - Y)
+   console.log(Z)
+
+   console.log(200 - 100)
+
+   // iii) * Multiplication Operator
+
+   let A = 20
+   let B = 30
+   let C = (A * B)
+   console.log(C)
+
+   console.log(100 * 200)
+
+  //  iv) / Division Operator 
+
+  let D = 50
+  let E = 5
+  let F = (D / E)
+  console.log(F)
+
+  console.log(20 / 6)
+
+  //  v) % Modulus Operator 
+
+  let p = 18
+  let q = 6
+  let r = (18 % 6)
+  console.log(r)
+
+  console.log(30 % 20)
+
+  // vi) ++ Incremant Operator
+
+   // a) Pre-Incremant Operator
+
+    let num = 1
+    console.log(++num)
+    console.log(num)
+
+   // b) Post-Incremant Operator
+
+    let abc = 5
+    console.log(abc++)
+    console.log(abc)
+
+   // vii) -- Decrement Operator
+
+    //  a) Pre-Decrement Operator
+
+    let xyz = 1
+    console.log(--xyz)
+    console.log(xyz)
+
+    // b) Post-Decrement Operator
+
+    pqr = 5
+    console.log(pqr--)
+    console.log(pqr)
+
+
+
+// B) Assignment Operator
+
+   let G = 50
+   console.log(G)
+
+   let j = 20
+   console.log(j)
+
+  //   i) += Add Assignment Operator
+ 
+    let R = 30
+    R = R + 30     // R+=30
+    console.log(R)
+
+  //   ii) -= Substract Assignment Operator
+
+    let P = 30
+    P = P - 20    // P-=20
+    console.log(P)
+
+    // iii) *= Multiplication Assignment Operator
+
+    let S = 20
+    S = S * 5    // S*=5
+    console.log(S)
+
+    // iv) /= Division Assignment Operator
+
+    let T = 40
+    T = T / 5   // T/=5
+    console.log(T)
+
+// C) Comparision Operator   
+
+    //  i)  == Equal To Operator
+
+    let U = 50
+    let V = 50
+    console.log(U==V)
+
+    let K = 100
+    let L = '100'
+    console.log(K==L)
+
+    let subject1_Name = "Marathi"
+    let subject2_Name = "marathi"
+    console.log(subject1_Name==subject2_Name)
+
+    // ii) === Strictly Equal To Operator
+
+    let J = 20
+    let H ='20'
+    console.log(J===H)  // check data type
+
+    // iii) != Not Equal To Operator
+
+    let jk = 30
+    let vk = 50
+    console.log(jk!=vk)
+
+    let AB = 40
+    let RP = '40'
+    console.log(AB!=RP)
