8103df897a8e431e95dde65f99745e3f5c759809
RajJadhav4140 <rajjadhav4140@gmail.com>
2025-07-11 20:30:56 +0530

    learn loops

---
+++ Day7/script.js
@@ -0,0 +1,46 @@
+//  * loop = repeat a block of code multiple times.
+
+// 1) While loop
+
+let count = 0;
+while(count <= 20){
+    console.log(count);
+    count++;
+}
+
+
+// print 0 to 100 odd number on console
+
+let number = 1;
+while (number <= 100){ 
+    console.log(number);
+    number = number + 2;
+}
+
+// print 0 to 100 even number on consloe
+
+let num = 0
+while(num <= 100){
+    console.log(num);
+    num = num + 2;
+}
+
+
+
+// Do While loop
+
+let number1 = 0
+
+do {
+    console.log(number1);
+    number1++;
+} while(number1 <= 10);
+  
+
+// // print 0 yo 100 odd number
+
+let n = 1;
+do {
+    console.log(n);
+    n += 2;
+} while(n <= 100);
