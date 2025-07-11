b1f298701026854f1700dca2ef225ea6fa113126
RajJadhav4140 <rajjadhav4140@gmail.com>
2025-07-11 20:24:12 +0530

    learn pop up box

---
+++ Day4/script.js
@@ -0,0 +1,24 @@
+// Pop up Box
+
+// 1 alert
+
+alert("Hello")
+
+alert("How are you")
+
+// 2 prompt
+
+let name = prompt("Enter Your Name")
+console.log(name)
+ 
+
+let number_one = prompt("Enter First Number")
+let number_two =prompt("Enter second number")
+
+console.log(number_one + number_two)
+
+console.log(typeof number_one)
+console.log(typeof number_two)
+
+confirm("are you sure")
+
