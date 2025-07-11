89d115e795037abe323be90b9749395f712deaba
RajJadhav4140 <rajjadhav4140@gmail.com>
2025-07-11 20:18:05 +0530

    learn data types and variable

---
+++ Day2/script.js
@@ -0,0 +1,80 @@
+// alert("please try again"
+
+// keyword(let, var , const)
+
+let name = "Raj"
+console.log(name)
+
+var address = "Tondal"
+console.log(address)
+
+const age = 20
+console.log(age)
+
+//    Data Type in JavaScript
+//    1. Primitive Data Type
+
+//  A) Number
+
+let Roll_Number = 40
+const PI = 3.14
+
+console.log(Roll_Number)
+console.log(PI)
+
+// B) String
+
+let CityName = "shirwal"
+let CollageName = "ATC_Collage_Bhor"
+
+console.log(CityName)
+console.log(CollageName)
+
+// C) Boolean
+
+let Is_Valid_Phone_Number = true
+let In_Valid_User = false
+
+console.log(Is_Valid_Phone_Number)
+console.log(In_Valid_User)
+
+// D) undefined
+
+let your_Name
+let city
+
+console.log(typeof your_Name)
+console.log(typeof city)
+
+// your_Name=jon
+// console.log(typeof your_Name)
+// console.log(your_Name)
+
+// E) Null
+
+let marks=" "
+let blood_group = " "
+console.log(typeof marks)
+console.log(marks)
+console.log(typeof blood_group)
+console.log(blood_group)
+
+// Non Primitive Data Type
+
+// A) Object
+let product = {}
+console.log(product)
+console.log( typeof product)
+
+// B) Array
+
+const arr = []
+console.log(arr)
+console.log(typeof arr)
+
+// C) Function
+
+let fun = ()=>{}
+console.log(fun)
+console.log(typeof fun)
+
