46a2a259c1416bb4b83d1769951a16864c63f569
RajJadhav4140 <rajjadhav4140@gmail.com>
2025-07-11 20:28:14 +0530

    leran Switch Case

---
+++ Day6/script.js
@@ -0,0 +1,189 @@
+// switch case
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let month = "january"
+
+switch(month){
+    case "january":
+        console.log("it is january")
+    
+    case "february":
+        console.log("it is february")   
+
+    case "march":
+        console.log("it is march") 
+
+    case "April":
+        console.log("it is April")
+
+    case "May":
+        console.log("it is May")
+
+    case "Jun":
+        console.log("it is Jun")
+
+    case "July":
+        console.log("it is July")
+    
+    case "Augest":
+        console.log("it is Augest")
+
+    case "September":
+        console.log("it is September")
+
+    case "October":
+        console.log("it is October")
+
+    case "November":
+        console.log("it is November")
+
+    case "December":
+        console.log("it is December")
+
+    
+
+}
+
+
+let number = 4
+
+switch(number){
+    case 0:
+        console.log("Zero")
+    case 1:
+        console.log("One")
+    case 2:
+        console.log("Two")
+    case 3:
+        console.log("Three")
+    case 4:
+        console.log("Four")
+    case 5:
+        console.log("Five")
+    case 6:
+        console.log("Six")
+    case 7:
+        console.log("Seven")
+    case 8:
+        console.log("Eight")
+    case 9:
+        console.log("Nine")
+}
+
+let Car_Name = "Thar"
+
+switch(Car_Name){
+    case "Tata Nexon" :
+        console.log("it is Thar");
+        break;
+
+    case "Toyota" :
+        console.log("it is Toyota");
+        break;
+        
+    case "Tesla Model 3" :
+        console.log("it is Tesla Model 3"); 
+        break;
+    
+    case "Kia" :
+        console.log("it is Kia");
+        break;
+
+    case "Hyundai Creta" :
+        console.log("it is Hyundai Creta");
+        break;
+
+    case "Maruti Suzuki Brezza" :
+        console.log("it is Maruti Suzuki Brezza");
+        break;
+
+    case "Thar" :
+        console.log("it is Tata Nexon");
+        break;
+
+    case "Mahindra Scorpio" :
+        console.log("it is Mahindra Scorpio");
+        break;
+
+    case "Tata Punch" :
+        console.log("it is Tata Punch");
+        break;
+
+    case "Maruti Suzuki Swift" :
+        console.log("it is Maruti Suzuki Swift");
+        break;
+
+    case "Hyundai i20" :
+        console.log("it is Hyundai i20");
+        break;
+
+    case "Mahindra XUV400 EV" :
+        console.log("it is Mahindra XUV400 Ev");
+        break;
+
+    default:
+        console.log("it is not available")
+}
+
+
+let fruit = prompt("Enter a fruit's Name: ")
+switch(fruit){
+    case "apple":
+        console.log("Apple are crunchy and delicious!");
+        break;
+
+    case "banana":
+        console.log(" Bananas are a great source of potassium!");
+        break;
+    
+    case "orange":
+        console.log("Orange are juicy and full vitamin C!");
+        break;
+
+    case "mango":
+        console.log("Mango are sweet and juicy!");
+        break;
+
+    case "grape":
+        console.log("Grape are small and sweet!");
+        break;
+
+    case "strawberry":
+        console.log("Strawberry are sweet and nutritious!");
+        break;
+
+    case "watermelon":
+        console.log("Watermelon are refreshing and hydrating!");
+        break;
+
+    case "pineapple":
+        console.log("Pineapple are tropical and delicious!");
+        break;
+
+    case "cherry":
+        console.log("Cherry are small and flavorful!");
+        break;
+
+    case "kiwi":
+        console.log("Kiwi is a small and fuzzy fruit!");
+        break;
+
+    case "lemon":
+        console.log("Lemon are sour and versatile");
+        break;
+
+    default:
+        alert(" Sorry Not avaliable Let's try again!");
+        console.log("Not avaliable Let's try again!");
+        break;
+}
+
+console.log("=== Thank you visit Again! ===");
