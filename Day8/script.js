ebda417527eb8949d2aff7e3fbb829b8103a781d
RajJadhav4140 <rajjadhav4140@gmail.com>
2025-07-14 16:52:56 +0530

    learn for loop

---
+++ Day8/script.js
@@ -0,0 +1,27 @@
+// For Loop
+
+// Que: print numbers from 1 to 9 using for loop.
+
+let i = 1;
+
+for(let i = 1 ; i < 10 ; i++){    
+    console.log(i);
+}
+
+ 
+// que: calculate sum of first 25 numbers using for loop.
+
+let sum = 0;
+for (i = 1 ; i <= 25 ; i++){
+    sum += i;
+}
+console.log("The sum of first 25 numbers is :" ,sum);
+
+
+// que: create table for 23 using for loop.
+
+let no = 23;
+
+for (let i = 1 ; i <= 10 ; i++){
+    console.log(no * i)
+}
