9108b27512aa129d78fa4d1253dfa21ab4604f95
RajJadhav4140 <rajjadhav4140@gmail.com>
2025-07-17 16:25:49 +0530

    learn array

---
+++ Day10/script.js
@@ -0,0 +1,20 @@
+// array
+
+let A = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(A);
+
+console.log(A[1]);
+console.log(A[2]);
+console.log(A[3]);
+
+console.log(A.length); //length the array.
+
+let month = ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+for(let i = 0 ; i < month.length ; i++){
+    console.log(month[i])
+}
+
+let Animals = ["Lion", "Tiger", "Elephant", "Monkey", "Butterfly", "Kangaroo", "Zebra", "Dog", "Cat", "horse", "fox"]
+for (let i = 0 ; i < Animals.length ; i++){
+    console.log(Animals[i])
+}
