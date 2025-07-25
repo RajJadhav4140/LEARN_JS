dee6d64f24dd604e9dcdfef70e8873814e0af630
RajJadhav4140 <rajjadhav4140@gmail.com>
2025-07-25 19:08:33 +0530

    Learn document object model

---
+++ Day16/script.js
@@ -0,0 +1,14 @@
+// dom ( Document Object Model)
+
+// A
+
+let result = document.getElementsByTagName("h1")
+
+console.log(result)
+
+// B
+
+let collage = document.getElementsByClassName("collage_name")
+
+console.log(collage)
+
